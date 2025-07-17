9fbbf31109582084a41a7398dba9a46e4d05aa87
ravnoorsingh <160342867+ravnoorsingh@users.noreply.github.com>
2025-07-17 20:11:22 +0530

    Update MongoDB connection details and add data upload functionality

--- src/constant/__init__.py
+++ src/constant/__init__.py
@@ -2,12 +2,12 @@ import os
 
 
 AWS_S3_BUCKET_NAME = "wafer-fault"
-MONGO_DATABASE_NAME = "pwskills"
+MONGO_DATABASE_NAME = "waferDB"
 MONGO_COLLECTION_NAME = "waferfault"
 
 
 TARGET_COLUMN = "quality"
-MONGO_DB_URL="mongodb+srv://imran:TdPLW9Ad0OzpSSD2@cluster0.fv0lm61.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGO_DB_URL="mongodb+srv://ravnoorsingh2003:986aqv1p4ctEjtBU@cluster0.egggmat.mongodb.net/?retryWrites=true&w=majority"
 
 
 MODEL_FILE_NAME = "model"
